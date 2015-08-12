c28af17120c2a5ce2ca5781d893865ad7bb6e1a3
Jeff Franco & Rick Hills <student@epicodus.com>
2015-08-12 09:59:16 -0700

    initial files

---
+++ src/anagramGenerator.php
@@ -0,0 +1,14 @@
+<?php
+    class anagramGenerator 
+    {
+        function makeAnagram($input)
+        {
+
+        	$input_array = str_split($input);
+        	var_dump($input_array); 
+
+
+        }
+
+    }
+?>
